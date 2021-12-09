7b1d9858185bebf9e2f43e84d18ea3f701eae258
Werken <garrettewens@live.com>
2021-12-09 12:34:14 -0600

    Add day9 text files

--- src/days/Day1.java
+++ src/days/Day1.java
@@ -1,9 +1,5 @@
 package days;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import models.FileReaderHelper;
